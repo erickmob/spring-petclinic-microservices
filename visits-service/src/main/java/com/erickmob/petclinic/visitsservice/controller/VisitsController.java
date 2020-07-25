29c8dc8d22960e36e28a49feca8deb90ea4dc161
erickmob <erickmob@gmail.com>
2020-07-25 16:39:00 -0300

    config testing on controllers

--- visits-service/src/main/java/com/erickmob/petclinic/visitsservice/controller/VisitsController.java
+++ visits-service/src/main/java/com/erickmob/petclinic/visitsservice/controller/VisitsController.java
@@ -1,10 +1,13 @@
-package com.erickmob.petclinic.visitsservice;
+package com.erickmob.petclinic.visitsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/")
 public class VisitsController {
@@ -12,11 +15,14 @@ public class VisitsController {
     @Autowired
     private Environment env;
 
+    @Value("${message:Hello default}")
+    private String message;
+
     @RequestMapping("/")
     public String home() {
         // This is useful for debugging
         // When having multiple instance of this service running at different ports.
         // We load balance among them, and display which instance received the request.
-        return "Hello from Visits Service running at port: " + env.getProperty("local.server.port");
+        return message+ "running at port: " + env.getProperty("local.server.port");
     }
 }
